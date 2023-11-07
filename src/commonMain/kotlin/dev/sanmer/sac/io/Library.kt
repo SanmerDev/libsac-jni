8e0873584ebc67afefa66bcc44dfa0ffb8ac5c6c
SanmerDev <sanmer@sanmer.dev>
2023-11-07 14:39:30 +0800

    Optimize `getLibrary`

--- src/commonMain/kotlin/dev/sanmer/sac/io/Library.kt
+++ src/commonMain/kotlin/dev/sanmer/sac/io/Library.kt
@@ -9,23 +9,23 @@ internal interface Library {
     fun load()
 
     companion object {
+        private const val PATH = "LIBSAC_JNI_PATH"
+        private const val TMP = "sac-jni"
+
         internal fun <T: Library> T.getLibrary(): String {
-            if (System.getenv("LIBSAC_JNI_PATH") != null) {
-                return System.getenv("LIBSAC_JNI_PATH")
+            System.getenv(PATH)?.let { path ->
+                return path
             }
 
-            val libraryURL = checkNotNull(javaClass.getResource("/${name}"))
-            if (libraryURL.protocol != "jar") {
+            val library = javaClass.getResource("/${name}")
+            if (library?.protocol != "jar") {
                 throw LinkageError("Load $name")
             }
 
-            val libsDir = Files.createTempDirectory("sac-jni").toFile()
-            libsDir.deleteOnExit()
-
+            val libsDir = Files.createTempDirectory(TMP).toFile()
             val libraryFile = File(libsDir, name)
-            libraryFile.deleteOnExit()
 
-            val inputStream = libraryURL.openStream()
+            val inputStream = library.openStream()
             val outputStream = FileOutputStream(libraryFile)
 
             inputStream.use { input ->
